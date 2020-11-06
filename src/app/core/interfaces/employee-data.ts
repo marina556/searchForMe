682ce058fbdb54b59daa20353c28c6a7fd6e6461
marina556 <marinamakram556@gmail.com>
2020-11-06 19:04:43 +0200

    add interfaces

---
+++ src/app/core/interfaces/employee-data.ts
@@ -0,0 +1,15 @@
+import { EmployeeJobStatuses } from './employee-job-statuses';
+import { Position } from './position';
+import { Department } from './department';
+
+export interface EmployeeData {
+  id: string;
+  fullName_FL: string;
+  fullName_SL: string;
+  hiringDate: string;
+  firstContractingSalary: string;
+  position: Position;
+  department: Department;
+  employeeJobStatuses: EmployeeJobStatuses;
+
+}
---
+++ src/app/core/interfaces/employee.ts
@@ -0,0 +1,5 @@
+import { EmployeeData } from './employee-data';
+
+export interface Employee {
+  data: EmployeeData[];
+}
